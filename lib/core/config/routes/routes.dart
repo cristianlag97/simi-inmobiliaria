281d5cb7793614c4b54fd55db1c9b0ac55a9be34
CristianLagCyttek <cristian.laguna@cyttek.com>
2025-09-05 14:55:30 -0500

    add assets

--- lib/core/config/routes/routes.dart
+++ lib/core/config/routes/routes.dart
@@ -1,4 +1,9 @@
 import 'package:go_router/go_router.dart';
+import 'package:simi_inmobiliaria/core/config/routes/screen/not_found_screen.dart';
+import 'package:simi_inmobiliaria/features/auth/presentation/screens/login_screen.dart';
+import 'package:simi_inmobiliaria/features/splash/presentation/splash_screen.dart';
+import 'package:simi_inmobiliaria/features/video/presentation/video_screen.dart';
+import 'package:video_player/video_player.dart';
 
 
 part 'app_routes.dart';
